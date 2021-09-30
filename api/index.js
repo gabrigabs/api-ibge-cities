88ea5bd18cfdda08275f0c4d666f6255879e931e
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-09-30 17:19:03 -0300

    API start

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+
+
+const app = express();
+const port = 3000;
+
+
+routes(app);
+app.listen(port, () => console.log(`server running on port ${port}`));
+
+module.exports = app;
