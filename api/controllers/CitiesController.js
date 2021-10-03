d3b7a172414c9fbfefc28ff85088fcd7d0c0db9b
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-10-03 20:21:34 -0300

    add validations

--- api/controllers/CitiesController.js
+++ api/controllers/CitiesController.js
@@ -6,12 +6,39 @@ class CitiesController{
 
     static async getCities(req, res) {
         try{
+
+            const uf = req.query.uf;
+            const name = req.query.name;
+            const state = req.query.state;
+            const region = req.query.region;
             const allCities = await services.getAllCities(req);
-            res.json(allCities);
-            return res.status(200);
+
+
+            if(uf == null){
+                if(!allCities.length){
+                    return res.status(404).json({message: "no city was found with the given parameters"});
+                }else{
+                    return res.status(200).json(allCities);
+                }
+                
+            }else{
+                if (uf !== uf.toUpperCase()) {
+                    return res.status(400).json({message: "invalid uf field, its value must be upper case"});
+                }else if(uf.length !== 2){
+                    return res.status(400).json({message: "the uf field must have only two letters"});
+                }else if(!allCities.length){
+                    return res.status(404).json({message: "no city was found with the given parameters"});
+                
+                }else{
+                    return res.status(200).json(allCities);
+                }
+            }
+
+
 
         }catch(error){
-            return res.status(500);
+            res.json(error);
+
 
         }
     }
--- api/services/Services.js
+++ api/services/Services.js
@@ -2,30 +2,26 @@ const FormateDataService = require('./FormateDataService')
 const formateDataService = new FormateDataService;
 class Services{
 
-    
-    async getAllCities(req){
+    async getAllCities(req, res){
         const  {uf, name, state, region} = req.query;
 
-
         if(uf !== undefined){
+ 
+            const filterUf =  await formateDataService.formateData(); 
+             return filterUf.filter(location => location.uf === uf);
 
-            this.uf = uf;
-            const filterUf =  await formateDataService.formateData();  
-            return filterUf.filter(location => location.uf === uf);
+
 
 
         }if(name !== undefined){
-            this.name = name;
             const filterName = await formateDataService.formateData(); 
             return filterName.filter(location => location.name.toLowerCase().includes(name.toLowerCase()));
 
         }if(state !== undefined){
-            this.state = state;
             const filterName = await formateDataService.formateData(); 
             return filterName.filter(location => location.state.toLowerCase().includes(state.toLowerCase()));
 
         }if(region !== undefined){
-            this.region = region;
             const filterName = await formateDataService.formateData(); 
             return filterName.filter(location => location.region.toLowerCase().includes(region.toLowerCase()));
 
